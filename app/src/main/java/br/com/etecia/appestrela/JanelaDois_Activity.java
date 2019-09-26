55e4cba553ea10030b46e9fb8aecbd323ff89d23
Guilhermaoandrade <guilherme.souza102@fatec.sp.gov.br>
2019-09-25 22:08:29 -0300

    Menu criado

--- app/src/main/java/br/com/etecia/appestrela/JanelaDois_Activity.java
+++ app/src/main/java/br/com/etecia/appestrela/JanelaDois_Activity.java
@@ -1,13 +1,55 @@
 package br.com.etecia.appestrela;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class JanelaDois_Activity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.janela_dois_layout);
+
+        toolbar = findViewById(R.id.ToolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mCadastrar:
+                startActivity(new Intent(getApplicationContext(),JanelaDois_Activity.class));
+                break;
+            case R.id.mAlterar:
+                Toast.makeText(getApplicationContext(), "Cliquei no Alterar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mExcluir:
+                Toast.makeText(getApplicationContext(), "Cliquei no Excluir", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mBuscar:
+                Toast.makeText(getApplicationContext(), "Cliquei no Buscar", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
+
